55a32cc971be342bc405ec4c37cc40a91a3b0a52
ManNjoro <elijohnmwoho@gmail.com>
2025-06-11 01:10:51 +0300

    Update UserTable.tsx: fix typo, add quotes around sortOrder string

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -9,24 +9,26 @@ interface User {
 }
 
 interface Props {
-    sortOrder: string;
+  sortOrder: string;
 }
 
-const UserTable = async ({sortOrder}: Props) => {
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
 
-  const sortedUsers = sort(users).asc(sortOrder === 'email' ? user => user.email : user => user.name)
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (user) => user.email : (user) => user.name
+  );
 
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
           <th>
-            <Link href='/users?sortOrder=name'>Name</Link>
+            <Link href="/users?sortOrder=name">Name</Link>
           </th>
           <th>
-            <Link href='/users?sortOrder=email'>Email</Link>
+            <Link href="/users?sortOrder=email">Email</Link>
           </th>
         </tr>
       </thead>
