efc93db290554d7f5109d097b15700257f85027d
ManNjoro <elijohnmwoho@gmail.com>
2025-06-30 23:22:38 +0300

    Added schema validation to users API routes

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 
 export function GET(request: NextRequest, {params}: {params: { id: number}}){
@@ -8,7 +9,8 @@ export function GET(request: NextRequest, {params}: {params: { id: number}}){
 
 export async function PUT(request: NextRequest, {params}: {params: { id: number}}){
     const body = await request.json()
-    if(!body.name) return NextResponse.json({error: 'Name is Required'}, {status: 400});
+    const validation = schema.safeParse(body)
+    if(!validation.success) return NextResponse.json(validation.error.errors, {status: 400});
     if (params.id > 10) return NextResponse.json({error: 'User not found'}, {status: 404});
     return NextResponse.json({id: 1, name: body.name})
 }
