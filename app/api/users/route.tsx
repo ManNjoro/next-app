e30889488001893dcb67b92e168b7cf067bc2847
ManNjoro <elijohnmwoho@gmail.com>
2025-07-02 01:06:14 +0300

    Refactor user API endpoints to use string IDs and update user data in database

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -11,11 +11,16 @@ export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = schema.safeParse(body)
     if (!validation.success) return NextResponse.json(validation.error.errors, {status: 400});
-    const user = await prisma.user.create({
+
+    const user = await prisma.user.findUnique({
+        where: { email: body.email}
+    })
+    if(user) return NextResponse.json({error: 'User already exists'}, {status: 400})
+    const newUser = await prisma.user.create({
         data: {
             name: body.name,
             email: body.email,
         }
     })
-    return NextResponse.json(user, {status: 201})
+    return NextResponse.json(newUser, {status: 201})
 }
