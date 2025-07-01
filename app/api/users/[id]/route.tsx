53c16ebb7aff4cb353543c093b8ba299368be101
ManNjoro <elijohnmwoho@gmail.com>
2025-07-02 00:55:30 +0300

    Refactor API routes for users endpoint: add Prisma integration, validate user data, and improve error handling

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
 
 
-export function GET(request: NextRequest, {params}: {params: { id: number}}){
-    if (params.id > 10) return NextResponse.json({error: 'User not found'}, {status: 404});
-    return NextResponse.json({id: 1, name: 'Mosh'})
+export async function GET(request: NextRequest, {params}: {params: { id: string}}){
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(params.id)}
+    })
+    if (!user) return NextResponse.json({error: 'User not found'}, {status: 404});
+    return NextResponse.json(user)
 }
 
 export async function PUT(request: NextRequest, {params}: {params: { id: number}}){
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,16 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import {prisma} from "@/prisma/client"
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Mosh'},
-        { id: 2, name: 'John'},
-    ])
+export async function GET(request: NextRequest) {
+    const users = await prisma.user.findMany()
+    return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = schema.safeParse(body)
-    if (!validation.success) return NextResponse.json(validation.error.errors, {status: 400})
-    return NextResponse.json({id: 1, name: body.name}, {status: 201})
+    if (!validation.success) return NextResponse.json(validation.error.errors, {status: 400});
+    const user = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+        }
+    })
+    return NextResponse.json(user, {status: 201})
 }
