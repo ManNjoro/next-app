bf162c898069ccfa5d84b873276a8a213d6c3e4f
ManNjoro <elijohnmwoho@gmail.com>
2025-07-02 19:55:13 +0300

    updated next-cloudinary and added next-auth dependencies

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,29 +1,39 @@
-"use client"
-import React, { useState } from 'react'
-import { CldImage, CldUploadWidget } from 'next-cloudinary'
+"use client";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
 
 interface CloudinaryResult {
-    public_id: string;
+  public_id: string;
 }
 
 const UploadPage = () => {
-    const [publicId, setPublicId] = useState('')
+  const [publicId, setPublicId] = useState("");
   return (
     <>
-    {publicId && 
-    <CldImage src={publicId} width={270} height={180} alt='A certificate' />}
-    <CldUploadWidget uploadPreset='nextapp' onSuccess={(result) => {
-        if(result.event !== 'success') return;
-        const info = result.info as CloudinaryResult;
-        setPublicId(info.public_id)
-    }}>
-        {({open}) => 
-        <button
-         className='btn btn-primary'
-         onClick={() => open()}>Upload</button>}
-    </CldUploadWidget>
-        </>
-  )
-}
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="A certificate" />
+      )}
+      <CldUploadWidget
+        uploadPreset="nextapp"
+        options={{
+            sources: ['local'],
+            multiple: false,
+            maxFiles: 5,
+        }}
+        onSuccess={(result) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
 
-export default UploadPage
+export default UploadPage;
