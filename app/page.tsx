38376c7be00783622217b5fe3e67ce66ce4612dd
ManNjoro <elijohnmwoho@gmail.com>
2025-07-04 23:32:06 +0300

    added Image component and updated next.config for remote image pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import ProductCard from "./components/ProductCard";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
   return (
     <main>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
+      <div className="relative h-screen">
+
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="alx"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={75}
+        priority
+        />
+        </div>
     </main>
   );
 }
