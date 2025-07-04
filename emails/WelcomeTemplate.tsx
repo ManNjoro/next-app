cf7e7fac8922fd8c9feb385183c63d1e4bb27cf7
ManNjoro <elijohnmwoho@gmail.com>
2025-07-04 22:01:03 +0300

    Updated WelcomeTemplate.tsx to use Tailwind and added resend package

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Html, Body, Container, Text, Link, Preview } from '@react-email/components'
+import { Html, Body, Container,Tailwind, Text, Link, Preview } from '@react-email/components'
 
 const WelcomeTemplate = ({name}: {name: string}) => {
   return (
     <Html>
         <Preview>Welcome aboard!</Preview>
-        <Body>
-            <Container>
-                <Text>Hello {name}</Text>
-                <Link href='https://codewithmosh.com'>www.codewithmosh.com</Link>
-            </Container>
-        </Body>
+        <Tailwind>
+            <Body>
+                <Container>
+                    <Text className='font-bold text-3xl'>Hello {name}</Text>
+                    <Link href='https://codewithmosh.com'>www.codewithmosh.com</Link>
+                </Container>
+            </Body>
+        </Tailwind>
     </Html>
   )
 }
